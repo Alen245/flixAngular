2e9c821926f89cb85f5e224855f5ad4c431938ad
Alen245 <akiyanovski24@gmail.com>
2023-08-09 12:49:46 -0400

    favorites add

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { FetchApiDataService } from '../fetch-api-data.service';
 import { MatDialog } from '@angular/material/dialog'; // Import MatDialog
 import { MovieDialogComponent } from '../movie-dialog/movie-dialog.component'; // Import your movie-dialog component
-import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-movie-card',
@@ -12,6 +11,7 @@ import { MatIconModule } from '@angular/material/icon';
 export class MovieCardComponent implements OnInit { // Add 'implements OnInit'
 
   movies: any[] = [];
+  favorites: any[] = []; 
 
   constructor(
     public fetchApiData: FetchApiDataService,
@@ -20,6 +20,7 @@ export class MovieCardComponent implements OnInit { // Add 'implements OnInit'
 
   ngOnInit(): void {
     this.getMovies();
+    this.getFavorites(); 
   }
 
   getMovies(): void {
@@ -44,5 +45,24 @@ export class MovieCardComponent implements OnInit { // Add 'implements OnInit'
         data: { movie: data }
       });
     }
+  }
+
+  addToFavorites(movie: any): void {
+    this.fetchApiData.addFavoriteMovie(movie._id).subscribe((response) => {
+      console.log('Added to favorites:', response);
+      this.getFavorites(); // Refresh the favorite movies list
+    });
+  }
+
+  // Method to check if a movie is already in favorites
+  isFavoriteMovie(movieId: string): boolean {
+    return this.favorites.some((favorite) => favorite._id === movieId);
+  }
+
+  // Method to get favorite movies
+  getFavorites(): void {
+    this.fetchApiData.getFavoriteMovies().subscribe((response) => {
+      this.favorites = response;
+    });
   }
 }

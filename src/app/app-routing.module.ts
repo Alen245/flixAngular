1534addef45bb617d6ed8d60208806df427a7f30
Alen245 <akiyanovski24@gmail.com>
2023-08-09 17:42:25 -0400

    fixed start view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { MovieCardComponent } from './movie-card/movie-card.component';
 import { UserProfileComponent } from './user-profile/user-profile.component'; 
 const routes: Routes = [
+  { path: '', redirectTo: 'welcome', pathMatch: 'full' },
   { path: 'movies', component: MovieCardComponent },
   { path: 'user-profile', component: UserProfileComponent }, // Add this route
   { path: '', redirectTo: '/movies', pathMatch: 'full' },
